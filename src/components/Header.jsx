c771c3717fdcbf6e704c16718c38c2d260d736d3
Ivan Garcia <ivan.garciafou@gmail.com>
2024-05-11 00:12:07 -0600

    Subiendo los primeros cambios

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Header() {
+    return(
+        <>
+            <section className="header">
+                <section className="options">
+                    <ul>
+                        <li>Home</li>
+                        <li>Academico</li>
+                        <li>Experiencia</li>
+                        <li>Contacto</li>
+                    </ul>
+                </section>
+            </section>
+        </>
+       
+    )
+}
